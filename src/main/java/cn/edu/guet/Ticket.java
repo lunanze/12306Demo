e64893c761c13de232f2ce99949e9da7171d1783
lnz <2559504567@qq.com>
2023-05-11 19:47:25 +0800

    首次提交

---
+++ src/main/java/cn/edu/guet/Ticket.java
@@ -0,0 +1,49 @@
+package cn.edu.guet;
+
+public class Ticket {
+    private String trainNumber;
+    private String fromStation;
+    private String toStation;
+    private String duration;
+    private String arrivalTime;
+
+    public String getTrainNumber() {
+        return trainNumber;
+    }
+
+    public void setTrainNumber(String trainNumber) {
+        this.trainNumber = trainNumber;
+    }
+
+    public String getFromStation() {
+        return fromStation;
+    }
+
+    public void setFromStation(String fromStation) {
+        this.fromStation = fromStation;
+    }
+
+    public String getToStation() {
+        return toStation;
+    }
+
+    public void setToStation(String toStation) {
+        this.toStation = toStation;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(String arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+}
